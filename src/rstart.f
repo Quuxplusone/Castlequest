463725e147cdb3877d6dfcb3d53cd2e6037f034f
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2021-03-10 17:55:25 -0500

    Modernize the random number generator.
    Eliminate the unknown dependencies `TOD` ("time of day")
    and `RDMIN` (seed the PRNG). Implement `RDM` to call the
    now-standard `RANDOM_NUMBER` library function instead.
    
    Thanks to @welash for pointing out that RDM should be a
    `REAL FUNCTION` instead of an `INTEGER FUNCTION` (although
    surprisingly it seems to work the same either way).

--- src/rstart.f
+++ src/rstart.f
@@ -1,14 +1,12 @@
 C---------------------------------------------------------------
 C
       SUBROUTINE RSTART(ISEED)
-      INTEGER   S1(2)
-      INTEGER*2 S2(4), I1(2), I2(2)
-      EQUIVALENCE (S1(1), S2(1)), (INT1, I1(1)), (INT2,I2(1))
+      RETURN
+      END
 C
-      CALL TOD(S1)
-      I1(2) = S2(1)
-      I2(2) = S2(4)
-      ISEED = INT1 + INT2
-      CALL RDMIN(ISEED)
+      REAL FUNCTION RDM(SEED)
+      REAL R
+      CALL RANDOM_NUMBER(R)
+      RDM = R
       RETURN
       END
